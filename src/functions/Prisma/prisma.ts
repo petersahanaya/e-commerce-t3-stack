328a0f083ca9b4988d6e09744bfec932b79df8cd
petersahanaya <petersahanaya09@gmail.com>
2023-03-08 16:47:26 +0900

    Fix Prisma Issues

--- src/functions/Prisma/prisma.ts
+++ src/functions/Prisma/prisma.ts
@@ -2,8 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
